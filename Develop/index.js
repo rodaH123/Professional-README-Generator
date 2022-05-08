50825d3d62553cd50c8914b683ced6006a45ad80
Roro <100329632+rodaH123@users.noreply.github.com>
2022-05-08 17:43:15 -0500

    Creating my list of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: 'input',
+       name:'projectTitle',
+       message:'What is your project title?'
+    },
+    {
+        type:  'input',
+        name: 'projectDescription',
+        message: 'What is your project Description?'
+    },
+    {
+        type: 'input',
+        name: 'Installation',
+        message: 'What is your installation?'
+    },
+
+    {   type: 'input',
+        name: 'usage information',
+        message: 'What is your usage information?'
+
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers)=> {
+        console.log('The title is'+ answers.projectTitle)
+        })
+ 
+}
 
 // Function call to initialize app
 init();
